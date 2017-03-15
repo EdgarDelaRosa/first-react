2265f18a16747bec3db6617d5636193c0ea6afc6
Edgar <Jarocho48@hotmail.com>
2017-03-14 20:58:31 -0700

    Update repo info

---
+++ src/components/courses/CourseForm.js
@@ -0,0 +1,51 @@
+import React, {PropTypes} from 'react';
+import TextInput from '../common/TextInput';
+import SelectInput from '../common/SelectInput';
+
+const CourseForm = ({allAuthors, course, errors, onSave, onChange}) => {
+  return (
+    <div>
+      <h1>Manage Course</h1>
+      <TextInput
+        name="title"
+        label="Title"
+        value={course.title}
+        onChange={onChange}
+        error={errors.title} />
+      <SelectInput
+        name="authorId"
+        label="Author"
+        value={course.authorId}
+        defaultOption="Select Author"
+        onChange={onChange}
+        options={allAuthors} />
+      <TextInput
+        name="category"
+        label="Category"
+        value={course.category}
+        onChange={onChange}
+        error={errors.category} />
+      <TextInput
+        name="length"
+        label="Length"
+        value={course.length}
+        onChange={onChange}
+        error={errors.length} />
+      <input
+        type="submit"
+        onClick={onSave}
+        className="btn btn-primary"/>
+    </div>
+
+  );
+};
+
+CourseForm.propTypes = {
+  allAuthors: PropTypes.arrayOf(PropTypes.object),
+  course: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
+  onSave: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired
+};
+
+export default CourseForm;
